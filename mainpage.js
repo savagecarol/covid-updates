c4d3fc6fa067f288907288987915c94d60bee1b7
kartikeya Sharma <karthiksharma1411@gmail.com>
2021-08-13 22:54:33 +0530

    lets do it

--- mainpage.js
+++ mainpage.js
@@ -1,5 +1,25 @@
 let p = document.getElementById("abcd");
-for(var i = 0 ; i < 1 ; i++)
-{
-    p.innerHTML+=p.innerHTML;
-}
+
+  fetch('https://api.covid19india.org/data.json')
+  .then((response) => {
+    return response.json()})
+  .then((data) => {
+    
+    var obj=data["statewise"];
+    for(var i = 1 ; i < obj.length ; i++)
+    {
+        p.innerHTML+= `  
+        <div class="card">
+            <div class="container">
+              <h2><b>${obj[i]["state"]} </b></h2> 
+              <h3>Active : ${obj[i]["active"]} </h3> 
+              <h3>Confirmed : ${obj[i]["confirmed"]}</h3>
+              <h3>Deaths : ${obj[i]["deaths"]}</h3>
+              <h3>Recovered : ${obj[i]["recovered"]} </h3>
+
+            </div>
+          </div>`;
+    } 
+    console.log();
+    })
+  .catch((err) => {})
